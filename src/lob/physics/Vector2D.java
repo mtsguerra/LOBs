68d37f683b94fb29e7500b17c28f5db7fa568b98
Matheus Guerra <mtsguerra9@gmail.com>
2026-04-25 18:09:15 +0100

    Adding directories

---
+++ src/lob/physics/Vector2D.java
@@ -0,0 +1,34 @@
+package lob.physics;
+
+public class Vector2D {
+    private final double x;
+    private final double y;
+
+    public Vector2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+
+    // Norma do vetor
+    public double norm() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    // Soma de dois vetores
+    public Vector2D add(Vector2D v) {
+        return new Vector2D(this.x + v.x, this.y + v.y);
+    }
+
+    // Escalonamento (multiplicar vetor por um escalar)
+    public Vector2D scale(double scalar) {
+        return new Vector2D(this.x * scalar, this.y * scalar);
+    }
+
+    // Produto escalar (dot product)
+    public double dot(Vector2D v) {
+        return (this.x * v.x) + (this.y * v.y);
+    }
+}
